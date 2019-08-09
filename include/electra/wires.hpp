8c3b5c0c52b67a2ac8fc49c2e39dc537b8eadf52
= <ruanformigoni@gmail.com>
2019-08-08 22:48:43 -0300

    Wires: Insertion, removal, query, size, json r/w

--- include/electra/wires.hpp
+++ include/electra/wires.hpp
@@ -12,40 +12,187 @@
 
 #include <vector>
 #include <memory>
+#include <algorithm>
+#include <memory>
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <iostream>
 
-namespace electra
+namespace electra::wire
 {
+  template<typename T>
+  using Container = std::vector<std::vector<std::pair<T,T>>>;
+
+  template<typename T>
+  using wire_const_iterator = typename Container<T>::const_iterator;
 
   template<typename T>
   class Wires
   {
+    /* Member data elements */
     private:
+      std::unique_ptr<Container<T>> wires;
     public:
-      Wires() = default;
-      template<typename U>
+    /* Constructors */
+      Wires();
+    /* Iterators */
+      wire_const_iterator<T> cbegin() const noexcept;
+      wire_const_iterator<T> cend() const noexcept;
+    /* Public Methods */
+      template<typename U = std::vector<std::pair<T,T>>>
       void add(U&& u) noexcept;
-      template<typename U, typename V>
-      V const& get(U&& a, U&& b) const noexcept;
-      template<typename U>
+      template<typename U = std::pair<T,T>>
+      auto get(U&& a, U&& b) noexcept;
+      template<typename U = std::pair<T,T>>
       void erase(U&& a, U&& b) noexcept;
+      template<typename U>
+      void write(U&& filename) const noexcept;
+      template<typename U>
+      void read(U&& filename) noexcept;
+      auto size() const noexcept;
+    private:
+    /* Private Methods */
+      auto json() const noexcept;
+      template<typename U>
+      auto find_if(U&& a, U&& b) noexcept;
+    /* Operators */
+      template<typename U>
+      friend std::ostream& operator<<(std::ostream& os, Wires<U> const& wires);
   };
 
+  /*
+   * Constructors
+   */
+  template<typename T>
+  Wires<T>::Wires()
+    : wires(std::make_unique<Container<T>>())
+  {
+  }
+
+  /*
+   * Iterators
+   */
+  template<typename T>
+  wire_const_iterator<T> Wires<T>::cbegin() const noexcept
+  {
+    return this->wires->cbegin();
+  }
+
+  template<typename T>
+  wire_const_iterator<T> Wires<T>::cend() const noexcept
+  {
+    return this->wires->cend();
+  }
+
+  /*
+   * Methods
+   */
   template<typename T>
   template<typename U>
   void Wires<T>::add(U&& u) noexcept
   {
+    this->wires->emplace_back(wire::encode(u.cbegin(), u.cend()));
   }
 
   template<typename T>
-  template<typename U, typename V>
-  V const& Wires<T>::get(U&& a, U&& b) const noexcept
+  template<typename U>
+  auto Wires<T>::get(U&& a, U&& b) noexcept
   {
+    return this->find_if(std::forward<U>(a),std::forward<U>(b));
   }
 
   template<typename T>
   template<typename U>
   void Wires<T>::erase(U&& a, U&& b) noexcept
   {
+    auto it {this->find_if(std::forward<U>(a),std::forward<U>(b))};
+
+    if( it == this->wires->cend() ) return;
+
+    this->wires->erase( it );
   }
 
-} // namespace electra
+  template<typename T>
+  template<typename U>
+  void Wires<T>::write(U&& filename) const noexcept
+  {
+    std::ofstream file{std::forward<U>(filename)};
+
+    if( file.good() )
+    {
+      file << this->json();
+    }
+  }
+
+  template<typename T>
+  template<typename U>
+  void Wires<T>::read(U&& filename) noexcept
+  {
+    using Json = nlohmann::json;
+
+    std::ifstream file{std::forward<U>(filename)};
+
+    if( file.good() )
+    {
+      this->wires = std::make_unique<Container<T>>();
+      Json j;
+      file >> j;
+      *this->wires = j.get<Container<T>>();
+    }
+
+  }
+
+  template<typename T>
+  auto Wires<T>::size() const noexcept
+  {
+    return this->wires->size();
+  }
+
+  /*
+   * Private Methods
+   */
+  template<typename T>
+  auto Wires<T>::json() const noexcept
+  {
+    using Json = nlohmann::json;
+    Json j;
+    for(auto it{this->cbegin()}; it != this->cend(); ++it)
+    {
+      j += *it;
+    }
+    return j;
+  }
+
+  template<typename T>
+  template<typename U>
+  auto Wires<T>::find_if(U&& a, U&& b) noexcept
+  {
+    return std::find_if(this->wires->cbegin(), this->wires->cend(),
+      [&](auto const& wire)
+      {
+        return ( a == *(wire.begin()) ) && ( b == *(wire.rbegin()) );
+      }
+    );
+  }
+
+  /*
+   * Operators
+   */
+  template<typename U>
+  std::ostream& operator<<(std::ostream& os, Wires<U> const& wires)
+  {
+    using Json = nlohmann::json;
+
+    Json j{wires.json()};
+
+    std::cout << "\033[92;1m * \033[mWires:" << std::endl;
+    for( auto const& wire : j[0] )
+    {
+      std::cout << "\t\033[92;1m * \033[m ";
+      std::cout << wire;
+      std::cout << std::endl;
+    }
+    return os;
+  }
+
+} // namespace electra::wire
--- test/include/electra/wires.cpp
+++ test/include/electra/wires.cpp
@@ -9,3 +9,42 @@
 */
 
 #include <electra/wires.hpp>
+
+TEST_CASE("Wires" "[wires]")
+{
+  electra::wire::Wires<int32_t> wires;
+  SECTION("Insertion")
+  {
+    wires.add({{3,1},{3,2},{3,3},{3,4}});
+    auto wire {wires.get({3,1},{3,4})};
+
+    REQUIRE(wire->size() == 3);
+  }
+
+  SECTION("Removal")
+  {
+    wires.add({{3,1},{3,2},{3,3},{3,4}});
+    wires.erase({3,1},{3,4});
+
+    auto wire {wires.get({3,1},{3,4})};
+
+    REQUIRE(wire == wires.cend());
+    REQUIRE(wires.size() == 0);
+    wires.add({{3,1},{3,2},{3,3},{3,4}});
+    REQUIRE(wires.size() == 1);
+  }
+
+  SECTION("Json Read")
+  {
+    wires.add({{3,1},{3,2},{3,3},{3,4}});
+    wires.add({{10,1},{9,1},{9,2},{9,3}});
+    wires.add({{3,1},{3,0},{3,-1},{3,-2}});
+    wires.add({{4,5},{5,5},{5,6},{5,7}});
+    wires.write("wires.json");
+
+    electra::wire::Wires<int32_t> _wires;
+    _wires.read("wires.json");
+
+    REQUIRE(_wires.size() == 4);
+  }
+}
